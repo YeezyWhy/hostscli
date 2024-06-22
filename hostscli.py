994655208ddea3599df53be34bc464dbd95cc1ec
Egor Gaev <gaev5941@gmail.com>
2024-06-22 23:44:11 +0300

    added some remote features, update function

--- hostscli.py
+++ hostscli.py
@@ -1,5 +1,6 @@
-import os, sys, traceback, ctypes, locale, json, warnings, platform, socket
+import os, sys, traceback, ctypes, locale, json, warnings, platform, socket, requests
 import paramiko
+import wget
 
 
 with warnings.catch_warnings():
@@ -22,38 +23,44 @@ with warnings.catch_warnings():
     HOSTSCLI_BIN = None
     HOSTSCLI_PATH = None
     if getattr(sys, 'frozen', False):
-        HOSTSCLI_BIN = os.path.basename(sys.executable)
         HOSTSCLI_PATH = os.path.dirname(os.path.abspath(sys.executable))
     elif __file__:
-        HOSTSCLI_BIN = os.path.basename(__file__)
         HOSTSCLI_PATH = os.path.dirname(os.path.abspath(__file__))
     LOCAL_IP = socket.gethostbyname(socket.gethostname())
     LOCALE_PATH = resource_path("locales")
+    WIN_BIN_PATH = resource_path("hostscli.exe")
+    MACOS_BIN_PATH = resource_path("hostscli_macos")
+    LINUX_BIN_PATH = resource_path("hostscli_linux")
     LOCALE_AVAILABLE = [json.load(open(f"{LOCALE_PATH}/{file}", encoding="utf-8"))['name'] for file in os.listdir(LOCALE_PATH) if os.path.splitext(file)[1] == ".json"]
-    LOCALE_DATA = json.load(open(f"{LOCALE_PATH}/{CULTURE}.json", encoding="utf-8"))
+    if (os.path.exists(f"{LOCALE_PATH}/{CULTURE}.json")):
+        LOCALE_DATA = json.load(open(f"{LOCALE_PATH}/{CULTURE}.json", encoding="utf-8"))
+    else:
+        LOCALE_DATA = json.load(open(f"{LOCALE_PATH}/en_US.json", encoding="utf-8"))
     LOCALIZATION_DATA = LOCALE_DATA['localization_data']
     OS_TYPE = platform.system()
-    VERSION = "01.1206.2024 Test Build"
-    SSH_CONFIG = ""
+    CURRENT_LOCALE = LOCALE_DATA['name']
+    LOCALE_AUTHOR = LOCALE_DATA['author']
+    VERSION = "02.2206.2024"
+    STATE = "Test"
+    # SSH_CONFIG = ""
 
 
     # HOSTSCLI SETTINGS BY SYSTEM
     match OS_TYPE:
+        
         case "Windows":
             # CONFIG_LOCATION = f"C:\\ProgramData\\hostscli\\hostscli.yaml"
             HOSTS = f"{os.environ['SystemRoot']}\\system32\\drivers\\etc\\hosts"
-        case "Darwin":
+        
+        case "Darwin" | "Linux":
             # CONFIG_LOCATION = "/etc/hostscli/hostscli.yaml"
             HOSTS = "/etc/hosts"
-        case "Linux":
-            # CONFIG_LOCATION = "/etc/hostscli/hostscli.yaml"
-            HOSTS = "/private/etc/hosts"
     
 
     # STATIC USAGE MESSAGES
     HELP_MSG = LOCALIZATION_DATA['HELP_MSG']
     HELP_MSG_ADD = LOCALIZATION_DATA['HELP_MSG_ADD']
-    HELP_MSG_CONFIG = LOCALIZATION_DATA['HELP_MSG_CONFIG']
+    # HELP_MSG_CONFIG = LOCALIZATION_DATA['HELP_MSG_CONFIG']
     HELP_MSG_EDIT = LOCALIZATION_DATA['HELP_MSG_EDIT']
     HELP_MSG_REMOVE = LOCALIZATION_DATA['HELP_MSG_REMOVE']
 
@@ -64,6 +71,37 @@ with warnings.catch_warnings():
 
     # FUNCTIONS
     
+
+    def get_actual_version_link() -> dict:
+        response = requests.get("https://api.github.com/repos/yeezywhy/hostscli/releases/latest").json()
+        assets = response['assets']
+        version_on_git = int(''.join(str(response['tag_name']).split('.')))
+        current_version = int(''.join(VERSION.split('.')))
+        if (current_version < version_on_git):
+            for asset in assets:
+                match OS_TYPE:
+                    case "Windows":
+                        if (asset['name'] == "hostscli_Windows.exe"):
+                            return {response['tag_name']: asset['browser_download_url']}
+                    
+                    case "Linux":
+                        if (asset['name'] == "hostscli_Linux"):
+                            return {response['tag_name']: asset['browser_download_url']}
+                    
+                    case "Darwin":
+                        if (asset['name'] == "hostscli_MacOS"):
+                            return {response['tag_name']: asset['browser_download_url']}
+
+
+    def download_update() -> None:
+        match OS_TYPE:
+            case "Windows":
+                output_name = "hostscli.exe"
+            
+            case "Darwin" | "Linux":
+                output_name = "hostscli"
+        wget.download(get_actual_version_link().keys[0], f"{HOSTSCLI_PATH}/{output_name}")
+
 
     # REMOTE
     def execute_command_on_remote_host(host, credentials, command, sudo = False) -> str:
@@ -96,15 +134,19 @@ with warnings.catch_warnings():
             sftp = paramiko.SFTPClient.from_transport(transport)
             match remote_system_type:
                 case "Windows":
-                    SSH_LOCALPATH = f"{HOSTSCLI_PATH}\\hostscli.exe"
+                    SSH_LOCALPATH = WIN_BIN_PATH
                     SSH_REMOTEPATH = "C:\\Windows\\Temp\\hostscli.exe"
-                case "Darwin" | "Linux":
-                    SSH_LOCALPATH = f"{HOSTSCLI_PATH}/hostscli"
+                case "Darwin":
+                    SSH_LOCALPATH = MACOS_BIN_PATH
+                    SSH_REMOTEPATH = "/tmp/hostscli"
+                case "Linux":
+                    SSH_LOCALPATH = LINUX_BIN_PATH
                     SSH_REMOTEPATH = "/tmp/hostscli"
             sftp.put(SSH_LOCALPATH, SSH_REMOTEPATH)
             sftp.close()
             transport.close()
-            execute_command_on_remote_host(host, credentials, f"chmod +x {SSH_REMOTEPATH}", False)
+            if (remote_system_type == "Linux" | "Darwin"):
+                execute_command_on_remote_host(host, credentials, f"chmod +x {SSH_REMOTEPATH}", False)
         except Exception:
             traceback.print_exc()
 
@@ -114,7 +156,7 @@ with warnings.catch_warnings():
 
 
     def is_admin() -> bool:
-        """ Checks user for administrator privilages """
+        """Checks user for administrator privilages"""
         try:
             return os.getuid() == 0
         except AttributeError:
@@ -149,7 +191,7 @@ with warnings.catch_warnings():
                     print(execute_command_on_remote_host())
                 elif (remote_system_type in ["Linux", "Darwin"]):
                     print(f"{host} hosts:\n")
-                    print(execute_command_on_remote_host(host, credentials, f"cd /tmp && ./hostscli add {source} {' '.join(targets)}"))
+                    print(execute_command_on_remote_host(host, credentials, f"cd /tmp && sudo ./hostscli add {source} {' '.join(targets)}", True))
 
 
     def edit_hosts(source1, targets1, source2, targets2, hosts, credentials) -> None:
@@ -168,9 +210,21 @@ with warnings.catch_warnings():
                 with open(HOSTS, 'w') as hosts_file:
                     hosts_file.write(content)
                     hosts_file.close()
+            else:
+                remote_system_type = get_remote_system_type(host, credentials)
+                print(replace_all(LOCALIZATION_DATA["INFO_MSG_SFTP_COPY"], { "{host}": host }))
+                add_hostscli_to_remote_host(host, credentials, remote_system_type)
+                print(INFO_MSG_DONE)
+                if (remote_system_type == "Windows"):
+                    print(f"{host} hosts:\n")
+                    print(execute_command_on_remote_host())
+                elif (remote_system_type in ["Linux", "Darwin"]):
+                    print(f"{host} hosts:\n")
+                    print(execute_command_on_remote_host(host, credentials, f"cd /tmp && sudo ./hostscli edit from {source1} {' '.join(targets1)} to {source2} {' '.join(targets2)}", True))
 
 
     def print_hosts(hosts, credentials) -> None:
+        """Prints hosts data"""
         for host in hosts:
             if (host in ['localhost', '127.0.0.1', LOCAL_IP]):
                 with open(HOSTS, 'r') as f:
@@ -204,6 +258,21 @@ with warnings.catch_warnings():
                 with open(HOSTS, 'w') as hosts_file:
                     hosts_file.write(content)
                     hosts_file.close()
+            else:
+                remote_system_type = get_remote_system_type(host, credentials)
+                print(replace_all(LOCALIZATION_DATA["INFO_MSG_SFTP_COPY"], { "{host}": host }))
+                add_hostscli_to_remote_host(host, credentials, remote_system_type)
+                print(INFO_MSG_DONE)
+                if (remote_system_type == "Windows"):
+                    print(f"{host} hosts:\n")
+                    print(execute_command_on_remote_host())
+                elif (remote_system_type in ["Linux", "Darwin"]):
+                    print(f"{host} hosts:\n")
+                    print(execute_command_on_remote_host(host, credentials, f"cd /tmp && sudo ./hostscli rm {source} {' '.join(targets)}", True))
+
+
+    if (get_actual_version_link() != None):
+        print(replace_all(LOCALIZATION_DATA['INFO_MSG_UPDATE'], { '{version}': {list(get_actual_version_link().keys())[0]} }))
 
 
     # APP MAIN CODE
@@ -219,12 +288,12 @@ with warnings.catch_warnings():
                             source = arguments[1]
                             targets = arguments[2::]
                             try:
-                                credentials = arguments[arguments.index('cred')+1]
+                                credentials = arguments[arguments.index('--cred')+1]
                             except Exception:
                                 credentials = None
                             try:
                                 if (credentials != None):
-                                    hosts = arguments[arguments.index('host')+1:arguments.index('cred'):]
+                                    hosts = arguments[arguments.index('--host')+1:arguments.index('--cred'):]
                                 else:
                                     hosts = ["localhost"]
                             except Exception:
@@ -239,7 +308,11 @@ with warnings.catch_warnings():
                         input(f"{LOCALIZATION_DATA['ERROR_MSG_ADMIN']}")
 
 
-                case "config" | "creds" | "-conf":
+                case "config" | "creds":
+                    pass
+
+
+                case "inventory" | "inv":
                     pass
 
 
@@ -257,12 +330,12 @@ with warnings.catch_warnings():
                             targets1 = arguments[source1_index+1:to_index]
                             targets2 = arguments[source2_index+1::]
                             try:
-                                credentials = arguments[arguments.index('cred')+1]
+                                credentials = arguments[arguments.index('--cred')+1]
                             except Exception:
                                 credentials = None
                             try:
                                 if (credentials != None):
-                                    hosts = arguments[arguments.index('host')+1:arguments.index('cred'):]
+                                    hosts = arguments[arguments.index('--host')+1:arguments.index('--cred'):]
                                 else:
                                     hosts = ["localhost"]
                             except Exception:
@@ -283,20 +356,25 @@ with warnings.catch_warnings():
                             source = arguments[1]
                             targets = arguments[2::]
                             try:
-                                credentials = arguments[arguments.index('cred')+1]
+                                credentials = arguments[arguments.index('--cred')+1]
                             except Exception:
                                 credentials = None
                             try:
                                 if (credentials != None):
-                                    hosts = arguments[arguments.index('host')+1:arguments.index('cred'):]
+                                    hosts = arguments[arguments.index('--host')+1:arguments.index('--cred'):]
                                 else:
                                     hosts = ["localhost"]
                             except Exception:
                                 hosts = ["localhost"]
-                            print(replace_all(LOCALIZATION_DATA['INFO_MSG_REMOVE'], 
-                                            { "{source}": source, "{targets}": ' '.join(targets), "{hosts}": ' '.join(hosts) }))
-                            remove_hosts(source, targets, hosts, credentials)
-                            print(INFO_MSG_DONE)
+                            question_answer = input(replace_all(LOCALIZATION_DATA['QST_MSG_REMOVE'], 
+                                                { "{source}": source, "{targets}": ' '.join(targets), "{hosts}": ' '.join(hosts) }))
+                            if (question_answer.lower() == 'y'):
+                                print(replace_all(LOCALIZATION_DATA['INFO_MSG_REMOVE'], 
+                                                { "{source}": source, "{targets}": ' '.join(targets), "{hosts}": ' '.join(hosts) }))
+                                remove_hosts(source, targets, hosts, credentials)
+                                print(INFO_MSG_DONE)
+                            else:
+                                pass
                         else:
                             print(f"{LOCALIZATION_DATA['ERROR_MSG_PARAM']}{HELP_MSG_REMOVE}")
                     else:
@@ -305,12 +383,12 @@ with warnings.catch_warnings():
 
                 case "print" | "show" | "-p":
                     try:
-                        credentials = arguments[arguments.index('cred')+1]
+                        credentials = arguments[arguments.index('--cred')+1]
                     except Exception:
                         credentials = None
                     try:
                         if (credentials != None):
-                            hosts = arguments[arguments.index('host')+1:arguments.index('cred'):]
+                            hosts = arguments[arguments.index('--host')+1:arguments.index('--cred'):]
                         else:
                             hosts = ["localhost"]
                     except Exception:
@@ -338,9 +416,16 @@ with warnings.catch_warnings():
 
 
                 case "--version" | "-v":
-                    print(f"hostscli version {VERSION}\nOS: {OS_TYPE}\nhosts location: {HOSTS}\navailable locales: {', '.join(LOCALE_AVAILABLE)}")
+                    print(f"{replace_all(LOCALIZATION_DATA['INFO_MSG_VERSION'], { '{version}': VERSION, '{state}': STATE, '{os_type}': OS_TYPE, '{hosts}': HOSTS, '{locales}': ', '.join(LOCALE_AVAILABLE), '{locale_name}': CURRENT_LOCALE, '{author}': LOCALE_AUTHOR } )}")
                 
                 
+                case "--update":
+                    if (get_actual_version_link() != None):
+                        download_update()
+                    else:
+                        print(LOCALIZATION_DATA['INFO_MSG_UPTODATE'])
+
+
                 case _:
                     print(replace_all(LOCALIZATION_DATA['ERROR_MSG_ARG'], 
                                          { "{function_argument}": function_argument }),HELP_MSG)
